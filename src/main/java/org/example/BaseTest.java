626d34c9d700e9516fc5d92dbc5dc65225cccef4
Sivaraman M <msivaraman1998@gmail.com>
2023-08-11 12:09:29 +0530

    second-check

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
     public static void main(String[] args) {
+
+
             System.setProperty("webdriver.chrome.driver","C:\\Users\\Sivaraman M\\Documents\\test drivers\\chromedriver_win32\\chromedriver.exe");
            WebDriver driver= new ChromeDriver();
             driver.manage().window().maximize();
